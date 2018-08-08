ea5d4e26e41d0151ef790da8d7600b3a4fdc37ed
Sean Shadle <SeanDShadle@gmail.com>
2018-08-07 20:47:59 -0500

    Assignment 8 Player Bar: buttons: completed

--- src/components/song.js
+++ src/components/song.js
@@ -19,7 +19,7 @@ class Song extends Component {
                 {this.state.hover ? (this.props.playing ? <td className="icon ion-md-pause" /> : <td className="icon ion-md-play-circle" />) : <td>{this.props.index + 1}</td>}
                 <td>{this.props.song.title}</td>
                 <td>{this.props.song.duration}</td>
-            </tr >
+            </tr>
         );
     }
 }
